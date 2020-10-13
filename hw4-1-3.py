87e280df677e67e5254f9945cd7e46046f0b64c4
Kyle Bautista <p21kbautista@fairfieldprep.org>
2020-10-13 08:24:17 -0400

    Add hw4-2-3

---
+++ hw4-1-3.py
@@ -0,0 +1,10 @@
+# Kyle Bautista (AMDG) 10/12/20
+
+first = int(input("Type a number to see if its a multiple of 2, 3, or 5: "))
+
+if first <= 2:
+    print("Its a multiple of 2.")
+elif first <= 3:
+    print("Its a multiple of 3.")
+else:
+    print("Its a multiple of 5.")
